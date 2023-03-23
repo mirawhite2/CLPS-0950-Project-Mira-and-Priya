119e72267b9e7561e07e954e187bd29074868a67
pbhanot1 <priya_bhanot@brown.edu>
2023-03-22 21:53:46 -0400

    Priya Bhanot latest version

---
+++ NotFace1.m
@@ -0,0 +1,12 @@
+%notface1 (input)
+%written on 3/15/23
+%by Priya Bhanot
+%this script creates a basic image that does NOT include a face
+
+notface1 = ones(10);
+notface1(2:3, 7:8) = zeros;
+notface1(5:6,5:6) = zeros;
+notface1(8:9, 3:8) = zeros;
+
+imagesc(notface1);
+disp(notface1)
